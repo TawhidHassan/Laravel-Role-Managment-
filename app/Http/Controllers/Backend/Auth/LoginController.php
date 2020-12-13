2c34efc2a479602e9b8255c8942e1fd33e7665c8
sifat007 <sifat27.ht@gmail.com>
2020-12-13 09:42:58 +0600

    debug and solve login with gaurd for multi authentication

--- app/Http/Controllers/Backend/Auth/LoginController.php
+++ app/Http/Controllers/Backend/Auth/LoginController.php
@@ -44,22 +44,23 @@ class LoginController extends Controller
         ]);
 
         // Attempt to login
-        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password],$request->remember)) {
+            // Redirect to dashboard
             // Redirect to dashboard
             session()->flash('success', 'Successully Logged in !');
-            return redirect()->intended(route('admin.dashboard'));
+            return redirect()->route('admin.dashboard');
         } else {
             // Search using username 
-            if (Auth::guard('admin')->attempt(['username' => $request->email, 'password' => $request->password], $request->remember)) {
+            if (Auth::guard('admin')->attempt(['username' => $request->email, 'password' => $request->password],$request->remember)) {
+                // Redirect to dashboard
                 session()->flash('success', 'Successully Logged in !');
-                return redirect()->intended(route('admin.dashboard'));
+                return redirect()->route('admin.dashboard');
             }
             // error
             session()->flash('error', 'Invalid email and password');
             return back();
         }
     }
-
 
     /**
      * logout admin guard
